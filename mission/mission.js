fb867c70d99ffafe2255f9253d0c20457058b952
mel-j-vickery01 <u157233473@byui.edu>
2025-05-03 10:33:39 -0600

    wasted time

--- mission/mission.js
+++ mission/mission.js
@@ -1,21 +1,23 @@
 let elem = document.querySelector('select');
 
-let img = document.querySelector('.logo');
-
 elem.addEventListener('change', changetheme);
 
+var image = document.querySelector('img');
 function changetheme(){
     let current = elem.value;
 
     if (current == 'dark'){
         document.body.className = 'dark';
         document.h2.className = 'dark'
-        document.img.src = 'byui-logo_white.png';
+        image.className = 'byui-logo_white.png';
+        image.src = 'byui-logo_white.png';
+        document.getElementById("logo").src='byui-logo_white.png'
+        document.getElementById("logo").className='byui-logo_white.png'
         // give body dark class
         // change img at bottom
     }else{
-        document.body.className = '';
-        document.h2.className = '';
+        document.body.className = 'light';
+        document.h2.className = 'light';
         document.img.src = 'byui-logo_blue.webp';
         //remove class on body
         // change img back to origanal 
