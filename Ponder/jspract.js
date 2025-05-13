cc546536f0d16f812186390d65c7fd8ef5e97d10
mel-j-vickery01 <u157233473@byui.edu>
2025-05-13 10:43:21 -0600

    adding an eventlistener to JS

--- Ponder/jspract.js
+++ Ponder/jspract.js
@@ -1,40 +1,13 @@
 // variables vs const
 const Pi = 3.14;
-let radius = 3;
+let area = 0;
 
+function circleArea(radius){
+    area = radius * radius * Pi;
+    return area
+}
 
-
-let area = radius * radius * Pi;
 
 console.log(area)
 
 radius = 20;
-
-area = radius * radius * Pi;
-
-console.log(area)
-// type exchange 
-
-const one = 3;
-const two = "2";
-
-let result = one * two
-
-console.log(result)
-result = one + parseInt(two)
-
-console.log(result)
-
-// scope
-let global = 'I\'m global'
-function example(){
-    let block = "I am block level or local";
-
-    console.log(block)
-};
-console.log(global)
-example();
-
-globel = "I am also global";
-
-console.log(global)
