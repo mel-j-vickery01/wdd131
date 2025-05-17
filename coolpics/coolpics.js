1c9106abb5014ceec2793f82bf189a6698817795
mel-j-vickery01 <u157233473@byui.edu>
2025-05-17 11:22:51 -0600

    add a lot of java code. lot of fixes in css and media querries: li removed bullet points. made header and footer go across entire screen

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,8 +1,27 @@
 const button = document.querySelector('button');
+const menuList = document.querySelector('ul');
+const gallery = document.querySelector('#gallery');
 
-button.addEventListener('click', showMenu)
+window.addEventListener('load', handleResize);
+window.addEventListener('resize', handleResize);
+button.addEventListener('click', showMenu);
+gallery.addEventListener('click', showModal(event.target.closest('img')));
+
 
 function showMenu(){
-    const menuList = document.querySelector('ul')
     menuList.classList.toggle("hide");
+}
+function handleResize(){
+    if (innerWidth >= 1001){
+        menuList.classList.remove("hide");
+    }else{
+        menuList.classList.add("hide");
+    }
+}
+function showModal(){
+    const img = document.querySelector('#modal img').src;
+    const modal = document.querySelector('#modal')
+    img.split('-')[0]+'-full.jpeg'
+    modal.style.display = 'block'
+    console.log('ShowModal is runnig')
 }
